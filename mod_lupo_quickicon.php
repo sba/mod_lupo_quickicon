f9f54aa23c5198ee1c4583f32928b27c7d1097bd
Stefan Bauer <sba@gmx.ch>
2020-05-17 10:02:33 +0200

    translate menu-title

--- mod_lupo_quickicon.php
+++ mod_lupo_quickicon.php
@@ -16,7 +16,7 @@ defined('_JEXEC') or die('Restricted access');
 	<ul class="nav nav-list">
 	<li>
 		<a href="<?php echo JRoute::_('index.php?option=com_lupo') ?>">
-			<span class="icon-smiley-happy"></span> <span class="j-links-link">Spiele aktualisieren</span>
+			<span class="icon-smiley-happy"></span> <span class="j-links-link"><?=JText::_('MOD_LUPO_QUICKICON_TITLE')?></span>
 		</a>
 	</li>
 	</ul>
